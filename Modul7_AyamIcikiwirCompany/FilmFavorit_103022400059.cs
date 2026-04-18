16bacccd9e28ddf356f5de47e9f00f0cf91aead4
naufal-ariq-rifqi <sagirisamaa@gmail.com>
2026-04-18 14:40:37 +0700

    menambahkan class FilmFavorit_103022400059

---
+++ Modul7_AyamIcikiwirCompany/FilmFavorit_103022400059.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_AyamIcikiwirCompany
+{
+    internal class FilmFavorit_103022400059
+    {
+
+        public string title { get; set; }
+        public string director  { get; set; }
+        public string year { get; set; }
+
+        public string genre { get; set; }
+        public double rating { get; set; }
+
+        public int durationMinutes { get; set; }
+
+        public bool isWatched { get; set; }
+
+
+        public static void ReadJSON()
+        {
+           string jsonString = File.ReadAllText("jurnal7_1_103022400059.json");
+            var data = JsonSerializer.Deserialize<FilmFavorit_103022400059>(jsonString);
+            Console.WriteLine($"Judul: {data.title} \ndirector: {data.director} \n year: {data.year} \n genre: {data.genre} \n " +
+                $"rating: {data.rating} \n duraion: {data.durationMinutes} \n iswatched: {data.isWatched}");
+        }
+    }
+}
--- Modul7_AyamIcikiwirCompany/Program.cs
+++ Modul7_AyamIcikiwirCompany/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul7_AyamIcikiwirCompany;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FilmFavorit_103022400059.ReadJSON();
+    }
+}
